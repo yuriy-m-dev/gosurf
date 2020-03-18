a4fbeb18cd66a9755f62cdded0a91b196d2f210d
yuriy-m-dev <fath66426@gmail.com>
2020-03-18 17:42:13 +0200

    added surf

--- app/js/main.js
+++ app/js/main.js
@@ -4,11 +4,25 @@ $(function() {
         fade: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-left.svg"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-right.svg"></button>',
-        asNavFor: '.slider-dots'
+        asNavFor: '.slider-dotshead'
     });
-    $('.slider-dots').slick({
+    $('.slider-dotshead').slick({
         asNavFor: '.header__slider',
         arrows: false,
         slidesToShow: 4
+    });
+
+    $('.surf-slider').slick({
+        slidesToShow: 4,
+        prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-right.svg"></button>',
+        asNavFor: '.slider-map'
+    });
+    $('.slider-map').slick({
+        slidesToShow: 8,
+        slidesToScroll: 1,
+        arrows: false,
+        asNavFor: '.surf-slider',
+        focusOnSelect: true
     });
 });
